fbd7a35985f36c9d541d94dcb5b8a947548fcba9
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-03-31 19:09:37 +0530

    `done the UI will give final touch later`

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <p className="bg-red-400">hello world</p>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8 px-4">
+        <div className="text-center mb-8">
+          <h1 className="text-5xl font-bold text-gray-800 mb-2">
+            AI Image Enhancer
+          </h1>
+          <p className="text-lg text-gray-500">
+            Upload your image and let AI enhance to in seconds!
+          </p>
+        </div>
+        <Home />
+        <div className="text-lg text-gray-500 mt-6">
+          Powered by @iampriyanshusingh
+        </div>
+      </div>
     </>
   );
 }
